d4826ea59fb03f508c54977d8c3849e19df1ae0d
ats.t.chiba <ats.t.chiba@gmail.com>
2018-01-19 17:10:55 +0900

    update

---
+++ blendee.core/src/org/blendee/util/dialect/postgresql/ReturningInserter.java
@@ -0,0 +1,104 @@
+package org.blendee.util.dialect.postgresql;
+
+import java.util.Objects;
+
+import org.blendee.jdbc.BConnection;
+import org.blendee.jdbc.BlendeeManager;
+import org.blendee.jdbc.ColumnMetadata;
+import org.blendee.jdbc.ContextManager;
+import org.blendee.jdbc.DataTypeConverter;
+import org.blendee.jdbc.PrimaryKeyMetadata;
+import org.blendee.jdbc.TablePath;
+import org.blendee.orm.DataObject;
+import org.blendee.sql.Updatable;
+import org.blendee.sql.ValueExtractor;
+import org.blendee.sql.ValueExtractors;
+import org.blendee.sql.ValueExtractorsConfigure;
+import org.blendee.support.Row;
+
+/**
+ * PK が単一項目で、シーケンス (serial) を使用しているテーブル用のユーティリティクラスです。
+ * @param <T> PK の型
+ * @author 千葉 哲嗣
+ */
+public class ReturningInserter<T> {
+
+	private final TablePath path;
+
+	private final String columnName;
+
+	private final ValueExtractor extractor;
+
+	/**
+	 * @param path
+	 */
+	public ReturningInserter(TablePath path) {
+		this.path = path;
+
+		BConnection connection = ContextManager.get(BlendeeManager.class).getConnection();
+
+		PrimaryKeyMetadata pkMetadata = connection.getPrimaryKeyMetadata(path);
+
+		String[] columnNames = pkMetadata.getColumnNames();
+		if (columnNames.length != 1) throw new IllegalStateException("PK の項目が複数あります table=[" + path + "]");
+
+		columnName = columnNames[0];
+
+		DataTypeConverter converter = ContextManager.get(BlendeeManager.class).getConfigure().getDataTypeConverter();
+		ValueExtractors extractors = ContextManager.get(ValueExtractorsConfigure.class).getValueExtractors();
+
+		for (ColumnMetadata metadata : connection.getColumnMetadatas(path)) {
+			if (columnName.equals(metadata.getName())) {
+				extractor = extractors.selectValueExtractor(
+					converter.convert(metadata.getType(), metadata.getTypeName()));
+				break;
+			}
+		}
+
+		throw new IllegalStateException();
+	}
+
+	/**
+	 * @param data INSERT 対象
+	 * @return PK
+	 */
+	public T insertAndGetSequencePK(DataObject data) {
+		Objects.requireNonNull(data);
+
+		TablePath myPath = data.getPrimaryKey().getTablePath();
+		if (!myPath.equals(path))
+			throw new IllegalStateException("data のテーブルが不正です table=[" + myPath + "]");
+
+		return insertAndGetSequencePKInternal(data);
+	}
+
+	/**
+	 * @param row INSERT 対象
+	 * @return PK
+	 */
+	public T insertAndGetSequencePK(Row row) {
+		Objects.requireNonNull(row);
+
+		TablePath myPath = row.getTablePath();
+		if (!myPath.equals(path))
+			throw new IllegalStateException("row のテーブルが不正です table=[" + myPath + "]");
+
+		return insertAndGetSequencePKInternal(row);
+	}
+
+	@SuppressWarnings("unchecked")
+	private T insertAndGetSequencePKInternal(Updatable data) {
+		Objects.requireNonNull(data);
+
+		if (!path.equals(this.path))
+			throw new IllegalStateException("INSERT 対象のテーブルが不正です table=[" + path + "]");
+
+		Object[] pk = new Object[] { null };
+		ReturningUtilities.insert(path, data, (result) -> {
+			if (pk[0] != null) throw new IllegalStateException("PK を指定した検索で、結果が複数あります PK=[" + pk[0] + "]");
+			pk[0] = extractor.extract(result, 1);
+		}, columnName);
+
+		return (T) pk[0];
+	}
+}
---
+++ blendee.core/src/org/blendee/util/dialect/postgresql/ReturningUtilities.java
@@ -0,0 +1,182 @@
+package org.blendee.util.dialect.postgresql;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import org.blendee.jdbc.BConnection;
+import org.blendee.jdbc.BPreparedStatement;
+import org.blendee.jdbc.BResult;
+import org.blendee.jdbc.BResultSet;
+import org.blendee.jdbc.BlendeeManager;
+import org.blendee.jdbc.ContextManager;
+import org.blendee.jdbc.TablePath;
+import org.blendee.orm.DataObject;
+import org.blendee.sql.Criteria;
+import org.blendee.sql.DeleteDMLBuilder;
+import org.blendee.sql.InsertDMLBuilder;
+import org.blendee.sql.Updatable;
+import org.blendee.sql.UpdateDMLBuilder;
+import org.blendee.support.Row;
+
+/**
+ * PostgreSQL の RETURNING を使用した DML を発行するユーティリティクラスです。
+ */
+public class ReturningUtilities {
+
+	/**
+	 * INSERT を行います。
+	 * @param data INSERT 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void insert(DataObject data, Consumer<BResult> consumer, String... columnNames) {
+		insert(data.getRelationship().getTablePath(), data, consumer, columnNames);
+	}
+
+	/**
+	 * INSERT を行います。
+	 * @param row INSERT 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void insert(Row row, Consumer<BResult> consumer, String... columnNames) {
+		insert(row.getTablePath(), row, consumer, columnNames);
+	}
+
+	/**
+	 * INSERT を行います。
+	 * @param path INSERT 対象テーブル
+	 * @param data INSERT 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void insert(TablePath path, Updatable data, Consumer<BResult> consumer, String... columnNames) {
+		Objects.requireNonNull(path);
+		Objects.requireNonNull(data);
+		Objects.requireNonNull(consumer);
+		checkColumnNames(columnNames);
+
+		BConnection connection = ContextManager.get(BlendeeManager.class).getConnection();
+		InsertDMLBuilder builder = new InsertDMLBuilder(path);
+		builder.add(data);
+
+		String sql = builder.toString() + " RETURNING " + String.join(", ", columnNames);
+
+		try (BPreparedStatement statement = connection.prepareStatement(sql)) {
+			builder.complement(statement);
+
+			try (BResultSet result = statement.executeQuery()) {
+				while (result.next()) {
+					consumer.accept(result);
+				}
+			}
+		}
+	}
+
+	/**
+	 * UPDATE を行います。
+	 * @param data UPDATE 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void update(DataObject data, Consumer<BResult> consumer, String... columnNames) {
+		update(data.getRelationship().getTablePath(), data, data.getPrimaryKey().getCriteria(), consumer, columnNames);
+	}
+
+	/**
+	 * UPDATE を行います。
+	 * @param row UPDATE 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void update(Row row, Consumer<BResult> consumer, String... columnNames) {
+		update(row.getTablePath(), row, row.getPrimaryKey().getCriteria(), consumer, columnNames);
+	}
+
+	/**
+	 * UPDATE を行います。
+	 * @param path UPDATE 対象テーブル
+	 * @param data UPDATE 対象
+	 * @param criteria UPDATE 条件
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void update(TablePath path, Updatable data, Criteria criteria, Consumer<BResult> consumer, String... columnNames) {
+		Objects.requireNonNull(path);
+		Objects.requireNonNull(data);
+		Objects.requireNonNull(criteria);
+		Objects.requireNonNull(consumer);
+		checkColumnNames(columnNames);
+
+		BConnection connection = ContextManager.get(BlendeeManager.class).getConnection();
+		UpdateDMLBuilder builder = new UpdateDMLBuilder(path);
+		builder.add(data);
+		builder.setCriteria(criteria);
+
+		String sql = builder.toString() + " RETURNING " + String.join(", ", columnNames);
+
+		try (BPreparedStatement statement = connection.prepareStatement(sql)) {
+			builder.complement(statement);
+
+			try (BResultSet result = statement.executeQuery()) {
+				while (result.next()) {
+					consumer.accept(result);
+				}
+			}
+		}
+	}
+
+	/**
+	 * DELETE を行います。
+	 * @param data DELETE 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void delete(DataObject data, Consumer<BResult> consumer, String... columnNames) {
+		delete(data.getRelationship().getTablePath(), data.getPrimaryKey().getCriteria(), consumer, columnNames);
+	}
+
+	/**
+	 * DELETE を行います。
+	 * @param row DELETE 対象
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void delete(Row row, Consumer<BResult> consumer, String... columnNames) {
+		delete(row.getTablePath(), row.getPrimaryKey().getCriteria(), consumer, columnNames);
+	}
+
+	/**
+	 * DELETE を行います。
+	 * @param path DELETE 対象テーブル
+	 * @param criteria DELETE 条件
+	 * @param consumer consumer
+	 * @param columnNames RETURNING で使用する項目
+	 */
+	public static void delete(TablePath path, Criteria criteria, Consumer<BResult> consumer, String... columnNames) {
+		Objects.requireNonNull(path);
+		Objects.requireNonNull(criteria);
+		Objects.requireNonNull(consumer);
+		checkColumnNames(columnNames);
+
+		BConnection connection = ContextManager.get(BlendeeManager.class).getConnection();
+		DeleteDMLBuilder builder = new DeleteDMLBuilder(path);
+		builder.setCriteria(criteria);
+
+		String sql = builder.toString() + " RETURNING " + String.join(", ", columnNames);
+
+		try (BPreparedStatement statement = connection.prepareStatement(sql)) {
+			builder.complement(statement);
+
+			try (BResultSet result = statement.executeQuery()) {
+				while (result.next()) {
+					consumer.accept(result);
+				}
+			}
+		}
+	}
+
+	private static void checkColumnNames(String... columnNames) {
+		if (columnNames.length == 0) throw new IllegalStateException("RETURNING 項目が 0 です");
+	}
+}
