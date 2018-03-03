dc40fa70d234d4fc2ae43c88d4eb5d7d3803a55c
ats.t.chiba <ats.t.chiba@gmail.com>
2018-03-03 17:31:59 +0900

    update

--- blendee.core/src/org/blendee/jdbc/ComposedSQL.java
+++ blendee.core/src/org/blendee/jdbc/ComposedSQL.java
@@ -1,6 +1,14 @@
 package org.blendee.jdbc;
 
+/**
+ * プレースホルダを持つ SQL 文と、プレースホルダにセットする値を持つものを表すインターフェイスです。
+ * @author 千葉 哲嗣
+ */
 public interface ComposedSQL extends PreparedStatementComplementer {
 
+	/**
+	 * このインスタンスが持つ SQL 文を返します。
+	 * @return SQL 文
+	 */
 	String sql();
 }
