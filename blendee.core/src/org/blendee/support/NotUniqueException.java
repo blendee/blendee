9483644e31de78be7940025d2e861c88f305c518
bitzer11 <ats.t.chiba@gmail.com>
2018-04-22 01:41:11 +0900

    update

--- blendee.core/src/org/blendee/support/NotUniqueException.java
+++ blendee.core/src/org/blendee/support/NotUniqueException.java
@@ -1,13 +1,11 @@
 package org.blendee.support;
 
 import org.blendee.jdbc.BlendeeException;
-import org.blendee.sql.Effector;
 
 /**
  * ユニークキーを使用した検索にもかかわらず、検索結果が複数件あった場合にスローされる例外です。
  * @author 千葉 哲嗣
  * @see Executor#willUnique()
- * @see Executor#willUnique(Effector...)
  */
 public class NotUniqueException extends BlendeeException {
 
