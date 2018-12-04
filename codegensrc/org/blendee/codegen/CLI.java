49d0341cec3c4ab78154939918b120cb45f5c4ff
bitzer11 <ats.t.chiba@gmail.com>
2018-12-04 18:51:38 +0900

    update

---
+++ codegensrc/org/blendee/codegen/CLI.java
@@ -0,0 +1,19 @@
+package org.blendee.codegen;
+
+/**
+ * {@link TableFacadeGenerator} 用 CLI
+ * @author 千葉 哲嗣
+ */
+public class CLI {
+
+	/**
+	 * --help
+	 * --url <jdbc url>
+	 * --user <jdbc user>
+	 * --pass <jdbc password>
+	 * --root <root package name>
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {}
+}
